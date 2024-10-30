32224bb8d92a2487d44bc3ec0269cb89e71df4d2
long14542 <121614259+long14542@users.noreply.github.com>
2024-10-30 19:44:28 +0700

    add logic

--- Outlook/app/src/main/java/vn/edu/usth/outlook/activities/LoginActivity.java
+++ Outlook/app/src/main/java/vn/edu/usth/outlook/activities/LoginActivity.java
@@ -1,6 +1,9 @@
 package vn.edu.usth.outlook.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +11,60 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 import vn.edu.usth.outlook.R;
+import vn.edu.usth.outlook.fragment.SignupFragment;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText usernameEditText, passwordEditText;
+    private MaterialButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        // Set padding for system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_root), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize views
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        // Set login button click listener
+        loginButton.setOnClickListener(view -> {
+            String username = usernameEditText.getText().toString().trim();
+            String password = passwordEditText.getText().toString().trim();
+
+            // Check if username or password is empty
+            if (username.isEmpty()) {
+                usernameEditText.setError("Please input username");
+            }
+            if (password.isEmpty()) {
+                passwordEditText.setError("Please input password");
+            }
+
+
+            if (!username.isEmpty() && !password.isEmpty()) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    // Handle signup redirection
+    public void goToSignUp(View view) {
+        Intent intent = new Intent(this, SignupFragment.class);
+        startActivity(intent);
     }
 }
+
